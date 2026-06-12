e6d59661b9ea2c73a732e6d67d8ea0c56cc6603d
Gauransh-Sharma-2005 <igauransh2005@gmail.com>
2026-06-12 17:18:05 +0530

    using the card component in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Food from './Food.jsx';
+import Card from './Card.jsx';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Footer></Footer>
       <Food></Food>
+      <Card></Card>
       </>
     );
 }
